53cfd8a5bffe18a318b224ec3c14cac2136952f8
Eshan Mewantha <eshanm@hsenidmobile.com>
2017-07-12 01:08:00 +0530

    Relative path issue in csv loading fixed

--- csv/read_csv.py
+++ csv/read_csv.py
@@ -2,9 +2,9 @@ __author__ = 'eshan'
 
 ########################################################################################################################
 
-import csv                                                                              # using pure python
+import csv                                                                           # using pure python
 
-with open('csv/iris_training.csv', 'r') as f:
+with open('iris_training.csv', 'r') as f:
     lines = csv.reader(f)
     for row in lines:
         print(', '.join(row))
@@ -12,7 +12,20 @@ with open('csv/iris_training.csv', 'r') as f:
 
 ########################################################################################################################
 
-import pandas as pd                                                                     # using pandas
+import pandas as pd                                                                  # using pandas
 
-df = pd.read_csv('csv/iris_training.csv')
+df = pd.read_csv('iris_training.csv')
 print(df)
+
+
+########################################################################################################################
+
+import os                                                                            # using pandas with path validation
+import pandas as pd
+
+if os.path.exists('iris_training.csv'):
+    df = pd.read_csv('iris_training.csv', index_col=0)
+print(df)
+
+
+########################################################################################################################
